7cb9352382dd012ad6eb7d96dcf083788366313e
Prajwal Gaonkar <164473062+OP-Prajwal@users.noreply.github.com>
2026-08-16 12:25:10 +0530

    fix: add red color to mail icon in navbar

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,7 +6,7 @@ export const Icons = {
   globe: (props: IconProps) => <GlobeIcon {...props} />,
   home: (props: IconProps) => <HomeIcon color="#3B82F6" {...props} />,
   blog: (props: IconProps) => <NotebookIcon color="#F97316" {...props} />,
-  email: (props: IconProps) => <MailIcon {...props} />,
+  email: (props: IconProps) => <MailIcon color="#EF4444" {...props} />,
   linkedin: (props: IconProps) => (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <title>LinkedIn</title>
